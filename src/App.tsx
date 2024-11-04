1ca2769b4f9f47913322070de71e202f71fd88c2
H4lfbeard <humbert.thomas.pro@gmail.com>
2024-11-04 10:52:45 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<div>
+				<img src="public/irobot.jpg" className="logo" alt="irobot" />
+			</div>
+			<h1>React & Express</h1>
+		</>
+	);
+}
+
+export default App;
